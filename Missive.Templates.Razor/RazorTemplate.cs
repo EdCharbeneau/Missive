273196950f585e5a6bacafd63fbcf52e43bb78b6
EdCharbeneau <ed.charbeneau@gmail.com>
2014-02-14 15:12:56 -0500

    Added razor template extension

---
+++ Missive.Templates.Razor/RazorTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missive.Templates.Razor
+{
+    public static class RazorTemplate
+    {
+        /// <summary>
+        /// Takes the model T and applies to a razor template.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model">Model</param>
+        /// <param name="template">Razor Template</param>
+        /// <returns></returns>
+        public static string ToEmailBody<T>(this T model, string template)
+        {
+            return RazorEngine.Razor.Parse(template, model);
+        }
+    }
+}
